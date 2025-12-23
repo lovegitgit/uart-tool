51aef8069bf1a29d9e9c0b59d68bca0d629a5e5d
co-co <co-co@no-reply.com>
2025-12-23 10:43:33 +0800

    bump version to v0.0.6

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(os.path.join(this_directory, "README.md"), encoding="utf-8") as f:
 
 setup(
     name='uart-tool',
-    version='0.0.5',
+    version='0.0.6',
     description='uart 工具',
     long_description=long_description,
     long_description_content_type='text/markdown',
